b55d1815dd340b20ea6372a4594c5eee467e17de
anders462 <anders462@gmail.com>
2016-07-01 12:04:38 -0700

    feature change

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ pin13.dir(mraa.DIR_OUT);
 
 var state = false;
 
-function toggle(){
+function toggleLed(){
   state = !state;
   pin13.write(state ? 1 : 0);
   console.log(state ? 1 : 0);
   setInterval(toggle,500);
 }
 
-toggle();
+toggleLed();
